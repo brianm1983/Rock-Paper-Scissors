31f2a4c29e4af2c4a35fbdf07bb55ad514a1c869
brianm1983 <brianmpetersen@outlook.com>
2023-12-13 13:33:38 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function playRound(playerSelection, computerSelection) {
+    //My code here!
+
+}
+
+const playerSelection ='rock';
+const computerSelection = getComputerChoice();
+const.log(playRound(playerSelection, computerSelection))
+
+function game() {
+    
+}
